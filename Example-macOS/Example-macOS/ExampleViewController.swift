820fee4e868a637135ffd5b466d9859f64422fb8
Vishal Gupta <vishalgupta7@gmail.com>
2022-08-09 22:56:02 +0530

    Adds a sample svg file to test the var color support.

--- Example-macOS/Example-macOS/ExampleViewController.swift
+++ Example-macOS/Example-macOS/ExampleViewController.swift
@@ -6,12 +6,32 @@
 //
 
 import Cocoa
+import Macaw
 
 class ExampleViewController: NSViewController {
-    
     override func awakeFromNib() {
         super.awakeFromNib()
-        self.view.wantsLayer = true
-        self.view.layer?.backgroundColor = .white
+        view.wantsLayer = true
+        view.layer?.backgroundColor = .white
+    }
+    
+    func getFileContent(resource: String, ofType type: String = "svg", inDirectory directory: String? = nil, fromBundle bundle: Bundle = Bundle.main) -> String? {
+        guard let fullpath = bundle.path(forResource: resource, ofType: type, inDirectory: directory),
+         let text = try? String(contentsOfFile: fullpath, encoding: .utf8) else {
+            print("Error parsing file")
+            return nil
+        }
+        return text
+    }
+    
+    @IBOutlet var svgView: SVGView!
+    
+    @IBAction func buttonTapped(_ sender: NSButton) {
+        guard var xml = getFileContent(resource: "CSS") else { return }
+        
+        let styles = SVGHelper.getAllVarColors(xmlText: xml)
+        SVGHelper.replaceColorVars(xml: &xml, colorNames: styles)
+        svgView.node = (try? SVGParser.parse(text: xml)) ?? Group()
     }
 }
+
